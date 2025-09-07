f9c00b0d9174d9e489474254307f33a898c44083
Caiosouzadevv <caioss20.cs@gmail.com>
2025-09-07 17:45:19 -0300

    [FEAT]New user addition feature

--- public/dist/index.js
+++ public/dist/index.js
@@ -1,1 +1,32 @@
-(()=>{var n;(n=document.getElementById("content")).innerHTML="<h1>Olá Mundo</h1>",n.innerHTML+="<h1>Olá Mundo</h1>",n.innerHTML+="<h1>Olá Mundo</h1>",n.innerHTML+="<h1>Olá Mundo</h1>",n.innerHTML+="<h1>Olá Mundo</h1>"})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.ts":
+/*!**********************!*\
+  !*** ./src/index.ts ***!
+  \**********************/
+/***/ (() => {
+
+eval("{var content = document.getElementById('content');\nvar button = document.querySelector('button[id=\"add\"]');\nbutton.addEventListener('click', function (event) {\n    event.preventDefault(); // Impede o submit do formulário\n    addEmployee();\n});\nfunction addEmployee() {\n    var fullName = document.querySelector('#fullName');\n    var register = document.querySelector('#register');\n    var admin = document.querySelector('input[name=\"admin\"]:checked');\n    var active = document.querySelector('#active');\n    if (!content || !fullName || !register || !admin || !active) {\n        alert('Algum campo está faltando!');\n        return;\n    }\n    content.innerHTML += createLine(fullName.value, +register.value, admin.value, active.checked);\n}\nfunction createLine(fullName, register, admin, active) {\n    return \"\\n   \\n        \".concat(fullName, \"</br>\\n        \").concat(register, \"</br>\\n        \").concat(admin, \"</br>\\n        \").concat(active ? 'Sim' : 'Não', \"</br>\\n\\n    \");\n}\n\n\n//# sourceURL=webpack:///./src/index.ts?\n}");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.ts"]();
+/******/ 	
+/******/ })()
+;
--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,42 @@
- function component() {
-const element = document.getElementById('content');
+let content = document.getElementById('content');
+const button: HTMLButtonElement = document.querySelector('button[id="add"]');
 
+button.addEventListener('click', function(event) {
+    event.preventDefault(); // Impede o submit do formulário
+    addEmployee();
+});
 
-    element.innerHTML = "<h1>Olá Mundo</h1>";
-    element.innerHTML += "<h1>Olá Mundo</h1>";
-    element.innerHTML += "<h1>Olá Mundo</h1>";
-    element.innerHTML += "<h1>Olá Mundo</h1>";
-    element.innerHTML += "<h1>Olá Mundo</h1>";
+function addEmployee() {
+    let fullName: HTMLInputElement = document.querySelector('#fullName');
+    let register: HTMLInputElement = document.querySelector('#register');
+    let admin: HTMLInputElement = document.querySelector('input[name="admin"]:checked');
+    let active: HTMLInputElement = document.querySelector('#active');
 
-    return element;
-  }
+    if (!content || !fullName || !register || !admin || !active) {
+        alert('Algum campo está faltando!');
+        return;
+    }
 
-component();
+    content.innerHTML += createLine(
+        fullName.value,
+        +register.value,
+        admin.value,
+        active.checked
+    );
+}
+
+function createLine(
+    fullName: string,
+    register: number,
+    admin: string,
+    active: boolean
+) {
+    return `
+   
+        ${fullName}</br>
+        ${register}</br>
+        ${admin}</br>
+        ${active ? 'Sim' : 'Não'}</br>
+
+    `;
+}
